1fee3ea7eab64d92e19a95d5f441cf75fcbe8bb3
Jacob Stone <jstone6@stu.jsu.edu>
2021-03-25 18:51:50 -0500

    Completed Part 1 of the code.

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 let friends = ['Colton', 'Dylan', 'Will', 'Aaron', 'Bryant'];
 
-for (var i=0;i<50;i++){
-    console.log(friends[i%friends.length] + ': ')
+var count = 99;
+
+for (var i=0;i<99;i++){
+    console.log(friends[i%friends.length] + ':');
+
+    if (count > 0){
+    
+
+        console.log(count + " line of code in the file, "
+            + count + " line of code; " + friends[i%friends.length] + 
+            " strikes one out, clears it all out " + --count 
+            + " line of code in the file.");
+
+        console.log("******");
+    }
     
 
 }
